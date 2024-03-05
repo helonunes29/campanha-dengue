d7e128dc61c30df1eacd382c525ad1f59bd210e3
helo <125596881+helonunes29@users.noreply.github.com>
2024-03-05 11:00:40 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+document.getElementById("conscientizacao").addEventListener("click", function() {
+    window.location.href = "conscientizacao.html";
+});
+
+document.getElementById("transmissao").addEventListener("click", function() {
+    window.location.href = "dicas.html";
+});
+
+document.getElementById("estatistica").addEventListener("click", function() {
+    window.location.href = "estatistica.html";
+});
